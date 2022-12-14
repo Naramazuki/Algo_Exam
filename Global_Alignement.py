453aa4fe04b627f32bdb3f272fc556f9d07204b0
Mathis REINE <103426705+Naramazuki@users.noreply.github.com>
2022-12-14 17:48:10 +0100

    Update Global_Alignement.py

--- Global_Alignement.py
+++ Global_Alignement.py
@@ -1,10 +1,6 @@
 """ TP3 where we code a global alignment program
     based on Needleman-Wunsch algorithm """
 
-
-
-
-
 def simple_display(seq_top, seq_left, score):
     """ Do a simple display """
     print("Seq_top:  {}\nSeq_left: {}\nScore: {}".format(seq_top[::-1], seq_left[::-1], score))
@@ -47,44 +43,34 @@ def nice_display(seq_top, seq_left, score):
     # Print everything!
     print(to_print)
 
-class Cell():
-    def __init__(self, score=None, prev_pos=None):
-        self.score = score
-        self.prev_pos = prev_pos
-
-
-
-
 class DynamicMatrix:
     """ Class to generate an empty matrix """
     def __init__(self, seq_top, seq_left, match, mismatch, indel):
-        # Init all "self" variables
-        self.seq_top = seq_top
+        # Self sequence 1
         self.seq_left = seq_left
+        # Self sequence 2
+        self.seq_top = seq_top
+        # Score system
         self.match = match
         self.mismatch = mismatch
         self.indel = indel
-        # Create the matrix of Cell()
-        self.matrix=[]        
-        for W in range(len(seq_left)+1):
+        # The Matrix is a list
+        self.matrix = []
+        # For each line +1 (the init line, so on LEFT sequence)
+        for i in range(len(self.seq_left) + 1):
+            # Add a new list
             self.matrix.append([])
-            liste=[]
-            for j in range(len(seq_top)+1):
-                c=Cell()
-                liste.append(c)
-            
-            self.matrix[W]=liste
-        
+            # For each column +1 (the init column)
+            for j in range(len(self.seq_top) + 1):
+                # Add a empty Cell to the last line,
+                # the list we just create
+                self.matrix[-1].append(Cell())
 
     # self representation for print
     def __repr__(self):
         # What will be returned
         return "Scores:\n{}\nPrev_pos:\n{}\n\n".format(self.print_scores(), self.print_prev_pos())
-    #compare two nucleotides
-    def compare(self,ntd_A,ntd_B):
-        if(ntd_A == ntd_B):
-            return self.match
-        return self.mismatch
+
     # self representation for print
     def print_scores(self):
         """ Output the values of the matrix """
@@ -163,124 +149,144 @@ class DynamicMatrix:
     def initialize(self):
         """ Initialize the matrix, i.e. fill the first line and column """
         # First cell is 0
-        self.matrix[0][0].score=0
-        self.matrix[0][0].prev_pos=[]
+        self.matrix[0][0].score = 0
+        # First line
+        for i in range(1, len(self.seq_top) + 1):
+            # Init the value: its position time indel value
+            self.matrix[0][i].score = self.indel*i
+            # Init the prev_pos: previous position in the line
+            self.matrix[0][i].prev_pos = [0, i-1]
+        # First column
+        for i in range(1, len(self.seq_left) + 1):
+            # Init the value: its position time indel value
+            self.matrix[i][0].score = self.indel*i
+            # Init the prev_pos: previous position in the column
+            self.matrix[i][0].prev_pos = [i-1, 0]
 
+    def compare(self, ntd_a, ntd_b):
+        """ Compare to nucleotides and return:
+            the match value if they are identical,
+            mismatch otherwise
+        """
+        # If the two nucleotides are the same
+        if ntd_a == ntd_b:
+            # the match value is returned
+            return self.match
+        # else, the mismatch value is returned
+        return self.mismatch
 
-        for i in range(1,len(self.seq_top)+1):
-            self.matrix[0][i].score=self.matrix[0][i-1].score+self.indel
-            self.matrix[0][i].prev_pos=[0,i-1]
-        for j in range(1,len(self.seq_left)+1):
-            self.matrix[j][0].score=self.matrix[j-1][0].score+self.indel
-            self.matrix[j][0].prev_pos=[j,0]
-
-        
     def fill_matrix(self):
         """ Fill-up the matrix """
-        for i in range(1,len(self.seq_left)+1):
-            for j in range(1,len(self.seq_top)+1):
-                self.matrix[i][j].prev_pos=[]
+        # For each cell of the matrix
+        for i in range(1, len(self.seq_left) + 1):
+            for j in range(1, len(self.seq_top) + 1):
+                # The score from the top is its value plus the indel score
+                top_score = self.matrix[i-1][j].score + self.indel
+                # Position of top
+                top_prev_pos = [i-1, j]
 
-                diag=self.matrix[i-1][j-1].score+self.compare(self.seq_top[j-1],self.seq_left[i-1])
-                gap_left=self.matrix[i][j-1].score+self.indel
-                gap_top=self.matrix[i-1][j].score+self.indel
-                maxi=max( diag,gap_top,gap_left)
-                self.matrix[i][j].score=maxi
-                if maxi==gap_top:
-                    self.matrix[i][j].prev_pos.append([i-1,j])
-                if maxi==gap_left:
-                    self.matrix[i][j].prev_pos.append([i,j-1])
-                if maxi==diag:
-                    self.matrix[i][j].prev_pos.append([i-1,j-1])
-    def global_alignment2(self,j,i,listetop,listeleft):
-            """ Make a global alignment of two sequences """
-            
-            intstop=j*i
-            
-            top=""
-            left=""
-            d=len(self.matrix[j][i].prev_pos)
-        
+                # The score from the top-left is its value plus:
+                # the match value if the nucleotides are the same,
+                # the mismatch value otherwise
+                top_left_score = self.matrix[i-1][j-1].score + self.compare(self.seq_left[i-1], self.seq_top[j-1])
+                # Position of top-left
+                top_left_prev_pos = [i-1, j-1]
 
-            
-        
-        
-            
+                # The score from the left is its value plus the indel score
+                left_score = self.matrix[i][j-1].score + self.indel
+                # Position of top
+                left_prev_pos = [i, j-1]
 
-            if i>0 and j>0 and intstop>0:
-                for n in range (d):
-                    v=self.matrix[j][i].prev_pos[n][0]
-                    w=self.matrix[j][i].prev_pos[n][1]
-                    print(n)
-                    if(w==i-1 and v==j-1):
-                        listeleft+=self.seq_left[j-1]
-                        listetop+=self.seq_top[i-1]
-                        i-=1
-                        j-=1
-                        top,left=self.global_alignment2(j,i,listetop,listeleft)
-                        
-                        listetop=listetop+top
-                        listeleft=listeleft+left
-            
-                    elif(w==i-1 and v==j):
-                        listeleft+="-"
-                        listetop+=self.seq_top[i-1]
-                        i-=1
-                        top,left=self.global_alignment2(j,i,listetop,listeleft)
-                        
-                        listetop=listetop+top
-                        listeleft=listeleft+left
-
-                        
-                    elif(w==i and v==j-1):
-                        listeleft+=self.seq_left[j-1]
-                        listetop+="-"
-                        j-=1
-                        top,left=self.global_alignment2(j,i,listetop,listeleft)
-                        
-                        listetop=listetop+top
-                        listeleft=listeleft+left
-
-            intstop-=1
-       # faire partie en iif qui va faire appel a deux focntion différentes selon si on se trouve dans le cas d'un embranchement ou danns le cas normal.
-       # argurment de la fonction sera la position en coordonnées de l'embranchement 
-
-            return(listetop,listeleft)
+                # This cell is the max of the three values
+                # and the position of this value
+                current_cell_score = max(top_score, top_left_score, left_score)
+                # Update the current cell in the matrix
+                self.matrix[i][j].score = current_cell_score
+                # Update the previous position
+                # Is the diagonal the best score?
+                # (start with diagonal because match/mismatch are better than gaps)
+                if top_left_score == current_cell_score:
+                    # Update current cell prev_pos
+                    self.matrix[i][j].prev_pos = top_left_prev_pos
+                # Else, is the top the best score?
+                elif top_score == current_cell_score:
+                    # Update current cell prev_pos
+                    self.matrix[i][j].prev_pos = top_prev_pos
+                # Else, is the left the best score?
+                elif left_score == current_cell_score:
+                    # Update current cell prev_pos
+                    self.matrix[i][j].prev_pos = left_prev_pos
+                # This should never occurs
+                else:
+                    # Error message, in case of...
+                    print("There is a bug at position {}".format([i, j]))
 
     def global_alignment(self):
         """ Make a global alignment of two sequences """
-        j,i=len(self.seq_left),len(self.seq_top)
-        
-        
-        seq_top=[]
-        seq_left=[]
-        score=self.matrix[j][i].score
-        listetop=""
-        listeleft=""
-        seq_top,seq_left=self.global_alignment2(j,i,listetop,listeleft)
-       
-        
-        return(seq_top,seq_left,score)
+        # Creation of two list which are going to contain
+        # the two sequences aligned reversed
+        al_seq_top = ""
+        al_seq_left = ""
+        # The end of the matrix is where the algorithm is starting
+        current_pos = [len(self.seq_left), len(self.seq_top)]
+        # Get the score
+        score = self.matrix[current_pos[0]][current_pos[1]].score
 
+        # Backtracking in the matrix
+        # The previous position
+        prev_pos = []
+        # While the beginning of the matrix is not reach
+        while prev_pos != [0, 0]:
+            # Get the previous position
+            prev_pos = self.matrix[current_pos[0]][current_pos[1]].prev_pos
+            # If the previous x position is the same
+            # as the current x position it is a insertion in seq_left
+            # so we put a gap
+            if current_pos[0] == prev_pos[0]:
+                al_seq_left += "-"
+            # Else, we write the previous character in the sequence
+            else:
+                al_seq_left += self.seq_left[prev_pos[0]]
+            # If the previous y position is the same
+            # as the current y position it is a insertion in seq_top
+            # so we put a gap
+            if current_pos[1] == prev_pos[1]:
+                al_seq_top += "-"
+            # Else, we write the previous character in the sequence
+            else:
+                al_seq_top += self.seq_top[prev_pos[1]]
+            # Current position is now the previous position
+            current_pos = prev_pos
+        return al_seq_top, al_seq_left, score
 
+class Cell:
+    """ Class to generate a cell of the matrix """
+    def __init__(self, score=None, prev_pos=None):
+        # Value of the cell
+        self.score = score
+        # Position (x, y) of the previous cell
+        self.prev_pos = prev_pos
+        # Put an empty array for prev_pos default value
+        if self.prev_pos is None:
+            self.prev_pos = []
 
-            
-
+    # self representation for print
+    def __repr__(self):
+        # Return the content of the cell
+        return "Val={}\nPrev_Pos={}\n".format(self.score, self.prev_pos)
 
 def main():
     """ The main of TP3"""
-    #mat = DynamicMatrix("ACGGCTATTCA", "ACTGTAGGGT", 2, -1, -2)
     mat = DynamicMatrix("ACGGCTAT", "ACTGTAG", 2, -1, -2)
-
     mat.initialize()
-    mat.fill_matrix()
-    
+    #mat.matrix[5][2].score = 42
+    #mat.matrix[5][2].prev_pos = [2, 4]
     print(mat)
-   
-
-
+    #mat.matrix[2][4].score = (5)
+    mat.fill_matrix()
+    print(mat)
     al_seq_top, al_seq_left, score = mat.global_alignment()
-    #nice_display(al_seq_top, al_seq_left, score)
+    nice_display(al_seq_top, al_seq_left, score)
 
 # Launch the main
 main()
